48abc98cab116f926fe786f6280e6cc1b816f8eb
Sneha Agarwal <snehaagarwal9034@gmail.com>
2025-09-15 20:24:23 +0530

    Add files via upload

---
+++ Backtracking.java
@@ -0,0 +1,17 @@
+public class Backtracking {
+    public static void printPermutation(String str, int idx, String perm){
+       if(str.length() == 0){
+           System.out.println(perm);
+           return;
+       }
+       for(int i=0; i<str.length(); i++){
+           char currChar = str.charAt(i); 
+           String remainingStr = str.substring(0, i) + str.substring(i+1); 
+           printPermutation(remainingStr, idx+1, perm+currChar); 
+       }
+       }
+       public static void main(String[] args) {
+           String str = "abc"; 
+           printPermutation(str, 0, ""); 
+       }    
+}
